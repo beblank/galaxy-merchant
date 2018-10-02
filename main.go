0e939e6339a9250e729b759fb22052866f44c469
Aditya Hadiwijaya <kulik2@ymail.com>
2018-10-02 10:37:08 +0700

    convert, validate, and calculate galaxy currency input

--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"html/template"
 	"log"
+	"metal/calculator"
+	"metal/converter"
+	"metal/validator"
 	"net/http"
+	"strconv"
 )
 
 type Data struct {
@@ -16,8 +20,22 @@ func galaxy(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 		r.ParseForm()
 	}
+	input := r.FormValue("input")
+	metal := r.FormValue("submit")
+	roman := converter.ConvertGalaxy(input)
+	validated := validator.IsValid(roman)
+	calculated := calculator.Calculate(roman, metal)
+	if validated {
+		result = input + " is " + strconv.FormatFloat(calculated, 'f', 1, 64) + " Credits"
+	} else {
+		result = "input not valid"
+	}
+	if input == "No Input" {
+		result = input
+	}
 	t, _ := template.ParseFiles("views/index.html")
-	t.Execute(w, nil)
+	d := Data{Result: result}
+	t.Execute(w, d)
 }
 
 func main() {
